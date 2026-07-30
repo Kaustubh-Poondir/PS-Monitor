905352d1ce6faf4852adf90c4b56520bae50ddec
Arnav Srivastava <136093544+arnvroot@users.noreply.github.com>
2026-07-30 11:24:41 +0530

    Delete Collectors  directory

--- Collectors /__init__.py
+++
@@ -1,4 +0,0 @@
-# makes the collector functions importable straight from the package,
-
-from Collectors.process_collector import collect_all_processes, collect_system_stats
-from Collectors.service_collector import collect_services, get_service_detail, ServiceInfo

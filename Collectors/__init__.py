ca8a652f3d57ed85370d7ff0cddfe2000a413220
Arnav Srivastava <136093544+arnvroot@users.noreply.github.com>
2026-07-30 11:28:59 +0530

    Initialize __init__.py with collector imports
    Add imports for collector functions in the package

---
+++ Collectors/__init__.py
@@ -0,0 +1,4 @@
+# helps collector functions to be imported straight from the package,
+
+from Collectors.process_collector import collect_all_processes, collect_system_stats
+from Collectors.service_collector import collect_services, get_service_detail, ServiceInfo
